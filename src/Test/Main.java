99426a0abce8e5be761a6170cec909c184735c43
Cristian Iacob <cristocea_24@yahoo.com>
2014-10-26 16:33:35 +0200

    Commit working class

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -2,11 +2,13 @@ package Test;
 
 /**
  * Created by Cristian on 10/25/2014.
+ *
+ * Main class
  */
 public class Main {
     public static void main(String [] args)
     {
-      //  System.out.println("Hello World 1");
+      //  Display Worker
         Work Cristian=new Work();
         Cristian.Nume = "Noname";
         Cristian.Prenume = "Prename";
