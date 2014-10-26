8a0d9280d387ff754b1f220ca9c992e36653ce19
Cristian Iacob <cristocea_24@yahoo.com>
2014-10-26 16:28:37 +0200

    Commit main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Created by Cristian on 10/25/2014.
+ */
+public class Main {
+    public static void main(String [] args)
+    {
+      //  System.out.println("Hello World 1");
+        Work Cristian=new Work();
+        Cristian.Nume = "Noname";
+        Cristian.Prenume = "Prename";
+        Cristian.CNP = 123151231;
+
+        System.out.println(Cristian.Nume+" "+Cristian.Prenume);
+    }
+}
