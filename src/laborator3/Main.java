f5410f10907756cffb9e55a6b960b2b431ff44c1
Cristian Iacob <cristocea_24@yahoo.com>
2014-11-01 01:20:46 +0200

    Sistem de fisiere2

--- src/laborator3/Main.java
+++ src/laborator3/Main.java
@@ -1,6 +1,7 @@
 package laborator3;
 
 import laborator3.utils.Directoare;
+import laborator3.utils.Fisiere;
 //import laborator3.utils.Sistem_fisiere;
 
 /**
@@ -11,6 +12,9 @@ public class Main {
     {
         Directoare Dir1=new Directoare("root");
         Dir1.inserare_fisier(200);
+
+        Fisiere fis1=new Fisiere("primul_fisier","Mata are cratima","txt");
+
 
     }
 
