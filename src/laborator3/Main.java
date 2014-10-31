17d899d62b8e4226dc753acdc278c34011ccce97
Cristian Iacob <cristocea_24@yahoo.com>
2014-11-01 00:56:57 +0200

    initializare main

--- src/laborator3/Main.java
+++ src/laborator3/Main.java
@@ -1,8 +1,18 @@
 package laborator3;
 
+import laborator3.utils.Directoare;
+//import laborator3.utils.Sistem_fisiere;
+
 /**
- * Created by Cristian on 10/30/2014.
+ * Clasa main pentru gestionarea Sistemului de fisiere
  */
 public class Main {
+    public void main(String [] args)
+    {
+        Directoare Dir1=new Directoare("root");
+        Dir1.inserare_fisier(200);
+
+    }
+
 
 }
