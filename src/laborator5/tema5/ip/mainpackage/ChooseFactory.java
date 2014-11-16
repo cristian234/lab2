8a6ed4f0ed4648eac4a6f297f9a0988bcf59fa9f
Cristian Iacob <cristocea_24@yahoo.com>
2014-11-16 23:27:43 +0200

    Tema5FinalCommit

--- src/laborator5/tema5/ip/mainpackage/ChooseFactory.java
+++ src/laborator5/tema5/ip/mainpackage/ChooseFactory.java
@@ -4,16 +4,12 @@ package laborator5.tema5.ip.mainpackage;
  * Choose the right factory to use, or return null
  */
 public class ChooseFactory {
-    public static AbstractFactoryGames factorySelection(String gameFactory)
-    {
-        AbstractFactoryGames newFactory=null;//initialize a factory with null
-        if(gameFactory.contains("Single"))
-        {
-            newFactory=new SinglePlayerGamesFactory();//create a single player factory
-        }
-        else if (gameFactory.contains("Multi"))
-        {
-            newFactory=new MultiPlayerGamesFactory();//create a multi player factory
+    public static AbstractFactoryGames factorySelection(String gameFactory) {
+        AbstractFactoryGames newFactory = null;//initialize a factory with null
+        if (gameFactory.contains("Single")) {
+            newFactory = new SinglePlayerGamesFactory();//create a single player factory
+        } else if (gameFactory.contains("Multi")) {
+            newFactory = new MultiPlayerGamesFactory();//create a multi player factory
         }
         return newFactory;
     }
